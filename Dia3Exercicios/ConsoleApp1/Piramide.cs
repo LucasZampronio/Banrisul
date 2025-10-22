529e45c36e2f9aa6e32a341e7f80f1b5d72be8d4
LucasZampronio <164106895+LucasZampronio@users.noreply.github.com>
2025-10-22 16:16:15 -0300

    Add files via upload

---
+++ Dia3Exercicios/ConsoleApp1/Piramide.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Piramide
+{
+    public static void Resposta()
+    {
+        Console.WriteLine("Digite a altura da pirâmide (1 a 8)");
+
+        int altura = Convert.ToInt32(Console.ReadLine());
+        int espaco = altura - 1;
+
+        string vazio = " ";
+
+
+        for (int i =0; i <= altura ; i++)
+        {
+         
+            for (int j = 0; j <= i; j++)
+            {
+                if(j == 0)
+                {
+                    Console.Write(string.Concat((Enumerable.Repeat(vazio, espaco))));
+                }
+                Console.Write("#");
+            }
+            espaco--;
+            Console.WriteLine();
+        }
+    }
+
+}
---
+++ Dia3Exercicios/ConsoleApp1/Tabuleiro.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class jogodavelha
+{
+    public static void Rodar()
+    {
+        string[,] tabuleiro = new string [3,3]{{ "   " , "   " , "   " },{ "   " , "   " , "   " },{ "   " , "   " , "   " } };
+        System.Console.WriteLine("Bem vindo ao jogo da velha!");
+        for(int turnos = 1; turnos <= 9; turnos+=2)
+
+            {
+            MostrarTabuleiro();
+
+            System.Console.WriteLine("É a vez do Jogador X. Selecione uma posição para jogar.");
+            System.Console.WriteLine("Digite a posição da linha desejada:");
+            int linhaJogadorX = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("Digite a posição da coluna desejada:");
+            int colunaJogadorX = Convert.ToInt32(Console.ReadLine());
+
+            if (linhaJogadorX == 9 || colunaJogadorX == 9) {
+                
+                break;
+                
+            }
+
+                tabuleiro[colunaJogadorX, linhaJogadorX] = " X ";
+
+            MostrarTabuleiro();
+
+
+            Console.WriteLine("É a vez do jogador O. selecione uma posição para jogar");
+            Console.WriteLine("Digite a posição da linha desejada");
+            int linhaJogadorO = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite a posição da coluna desejada");
+            int colunaJogadorO = Convert.ToInt32(Console.ReadLine());
+            tabuleiro[colunaJogadorO,linhaJogadorO] = " O ";
+
+            if (linhaJogadorO == 9 || colunaJogadorO == 9)
+            {
+                break;
+            }
+
+
+        }
+
+        if (tabuleiro[0,0] == " O " && tabuleiro[0,1] == " O " && tabuleiro[0,2] == " O ")
+        {
+            Console.WriteLine("Parabens! Voce venceu!");
+        }
+
+
+
+
+        void MostrarTabuleiro()
+        {
+            Console.WriteLine($"{tabuleiro[0, 0]}|{tabuleiro[0, 1]}|{tabuleiro[0, 2]}");
+            Console.WriteLine("---+---+---");
+            Console.WriteLine($"{tabuleiro[1, 0]}|{tabuleiro[1, 1]}|{tabuleiro[1, 2]}");
+            Console.WriteLine("---+---+---");
+            Console.WriteLine($"{tabuleiro[2, 0]}|{tabuleiro[2, 1]}|{tabuleiro[2, 2]}");
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
