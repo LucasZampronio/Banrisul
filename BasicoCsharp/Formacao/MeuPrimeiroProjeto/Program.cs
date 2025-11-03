e9efdf7276b13337a376a7bf0469f9437f80c866
LucasZampronio <164106895+LucasZampronio@users.noreply.github.com>
2025-11-03 17:28:15 -0300

    Add files via upload

---
+++ BasicoCsharp/Formacao/MeuPrimeiroProjeto/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto
+{
+    class App
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Nenhum argumento foi passado.");
+            }
+            else
+            {
+                Console.WriteLine($"Você passou {args.Length} argumento(s):");
+                int indice = 0;
+                foreach (var arg in args)
+                {
+                    Console.WriteLine($"[{indice}] {arg}");
+                    indice++;
+                }
+            }
+        }
+    }
+}
