7a06d8194ee50a23a4d2ba0b52a8959e1fe9e20a
LucasZampronio <164106895+LucasZampronio@users.noreply.github.com>
2025-11-03 17:30:30 -0300

    Add files via upload

---
+++ BasicoCsharp/Formacao/MinhaLib/Class1.cs
@@ -0,0 +1,32 @@
+﻿namespace MinhaLib.Matematica
+{
+    public class Calculadora
+    {
+        public int Somar(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtrair(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiplicar(int a, int b)
+        {
+            return a * b;
+        }
+        
+        public int Dividir(int a, int b)
+        {
+            if (b != 0)
+            {
+                return a / b;
+            }
+            else
+            {
+                throw new Exception("Divisão por zero não é permitida.");
+            }
+        }
+    }
+}
